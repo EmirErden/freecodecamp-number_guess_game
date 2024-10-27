36e0bd6931b02708ebbbae2cea8d9c80709ff93c
Emir Erdeb <emirerden123@gmail.com>
2024-10-27 19:14:30 +0000

    feat: Added player welcome messages

--- number_guess.sh
+++ number_guess.sh
@@ -1,7 +1,18 @@
 #!/bin/bash
+PSQL="psql --username=freecodecamp --dbname=number_guess -t --no-align -c"
+RANDOM_NUMBER=$((1 + RANDOM % 1000 ))
 
-RANDOM_NUMBER=$((1 + RANDOM % 100 ))
 
 echo Enter your username: 
 read USERNAME
-echo $RANDOM_NUMBER
+#Checking user info
+USER_INFO=$($PSQL "Select * From users Where username='$USERNAME'")
+#echoing sentences based on USER_INFO
+if [[ -z $USER_INFO ]]; then
+  echo "Welcome, $USERNAME! It looks like this is your first time here."
+fi
+
+echo Guess the secret number between 1 and 1000:
+read NUMBER_GUESS
+#adding while loop for the game
+while [[ $USERNAME != $RANDOM_NUMBER
