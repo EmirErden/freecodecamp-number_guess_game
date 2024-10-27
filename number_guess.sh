812ed255b0591c1971f11daec81c81d6fd2be84c
Emir Erdeb <emirerden123@gmail.com>
2024-10-27 20:15:14 +0000

    feat: Finishing the project

--- number_guess.sh
+++ number_guess.sh
@@ -3,7 +3,6 @@ PSQL="psql --username=freecodecamp --dbname=number_guess -t --no-align -c"
 RANDOM_NUMBER=$((1 + RANDOM % 1000 ))
 #Variables
 GAMES_PLAYED=0
-BEST_GAME=0
 NUMBER_OF_GUESSES=0
 
 echo Enter your username: 
@@ -13,15 +12,15 @@ USER_INFO=$($PSQL "Select * From users Where username='$USERNAME'")
 #echoing sentences based on USER_INFO
 if [[ -z $USER_INFO ]]; then
   echo "Welcome, $USERNAME! It looks like this is your first time here."
+  ADDING_USER=$($PSQL "Insert Into users(username) VALUES('$USERNAME')")
 else
-  $GAMES_PLAYED=$($PSQL "Select games_played From users Where username='$USERNAME'")
-  $BEST_GAME=$($PSQL "Select best_game From users Where username='$USERNAME'")
+  GAMES_PLAYED=$($PSQL "Select games_played From users Where username='$USERNAME'")
+  BEST_GAME=$($PSQL "Select best_game From users Where username='$USERNAME'")
   echo "Welcome back, $USERNAME! You have played $GAMES_PLAYED games, and your best game took $BEST_GAME guesses."
 fi
 #Game logic
 echo Guess the secret number between 1 and 1000:
 read NUMBER_GUESS
-echo $RANDOM_NUMBER
 NUMBER_OF_GUESSES=1
 #adding while loop for the game
 while [[ $NUMBER_GUESS != $RANDOM_NUMBER ]]; do
@@ -38,6 +37,15 @@ while [[ $NUMBER_GUESS != $RANDOM_NUMBER ]]; do
   fi
   read NUMBER_GUESS
 done
+
+USER_BEST_GAME=$($PSQL "Select best_game From users Where username='$USERNAME'")
+
+if [[ $NUMBER_OF_GUESSES -lt $USER_BEST_GAME ]]; then
+  CHANGE_BEST_GAME=$($PSQL "Update users Set best_game=$NUMBER_OF_GUESSES WHere username='$USERNAME'")
+fi
+USER_GAMES_PLAYED=($PSQL "Select games_played From users where username='$USERNAME'")
+((USER_GAMES_PLAYED++))
+CHANGE_GAMES_PLAYED=$($PSQL "Update users Set games_played=$USER_GAMES_PLAYED Where username='$USERNAME'")
 
 echo "You guessed it in $NUMBER_OF_GUESSES tries. The secret number was $RANDOM_NUMBER. Nice job!"
 
